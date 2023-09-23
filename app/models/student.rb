b98f3de13cd9e14a10842369317d71c2f2bdd9a5
Jesher Minelli <jesherdevsk8@gmail.com>
2023-09-23 14:57:11 -0300

    add validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 class Student < ApplicationRecord
   has_secure_password
 
+  normalizes :email, with: ->(email) { email.strip.downcase }
+
   has_many :enrollments, as: :enrollable
+
+  validates :name, :email, presence: true
+  validates :password_digest, presence: true, length: { maximum: 255 }
+  validates :email, uniqueness: true,
+                    format: { with: /\A^[a-z0-9._-]+@[a-z0-9]+\.[a-z]{2,3}+(\.[a-z]{2,3})?(\.[a-z]{2})?\z/,
+                              message: 'must be a valid email address' }
 end
